a366fc0040060ba7511e373531904e0300b0a5fb
masonpam <masonp@seattleu.edu>
2019-11-08 10:07:00 -0800

    midterm code

--- lab6_1.py
+++ lab6_1.py
@@ -1,22 +1,24 @@
+#Changed to return true or false
 def divisible_by_7(num):
    if num %7 == 0:
-      return num /7
-   else:
-      print("not divisble by 7")
-      return None 
+      return True 
+
+   return False  
 
 def compare_it(x, y):
-   if not isinstance(x, int) and not isinstance(y, int):
+#Changed "and" to "or"
+   if not isinstance(x, int) or not isinstance(y, int):
       return False
    if x != y:
       return False 
    if x < 0 and y < 0:
       return False
-   else:
-      return True
+  #Took away else  
+   return True
 
-def keyword_counter(word_list, logic, test): 
-   data = test
+def keyword_counter(word_list, logic, test):
+#Replaced "data" with "text" 
+   text = test
    if not logic:
       with open(test, "r") as f:
          text = f.read().replace("\n", " ")
